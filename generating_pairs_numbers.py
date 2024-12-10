c1a3408266973140a3ce4780366593bc1aebf21a
Jack Palmer <jack.palmer@fwcsstudents.org>
2024-12-10 12:31:44 -0500

    Update generating_pairs_numbers.py

--- generating_pairs_numbers.py
+++ generating_pairs_numbers.py
@@ -1,10 +1,16 @@
 import pygame
 import random
 
-# Generate pairs of symbols (for a memory matching game)
+# Generate pairs of number symbols (for a memory matching game)
 def generate_pairs_numbers():
-    symbols_face_up = [pygame.image.load("1-8_numbers_images/1.jpg").convert(), pygame.image.load("1-8_numbers_images/2.jpg").convert(), pygame.image.load("1-8_numbers_images/3.jpg").convert(),
-                       pygame.image.load("1-8_numbers_images/4.jpg").convert(), pygame.image.load("1-8_numbers_images/5.jpg").convert(), pygame.image.load("1-8_numbers_images/6.jpg.jpg").convert(),
-                       pygame.image.load("1-8_numbers_images/7.jpg").convert(), pygame.image.load("1-8_numbers_images/8.jpg").convert()] * 2  # 8 unique symbols, each appearing twice
+    symbols_face_up = [ pygame.image.load("1-8_numbers_images/1.jpg").convert(),
+                                pygame.image.load("1-8_numbers_images/2.jpg").convert(),
+                                pygame.image.load("1-8_numbers_images/3.jpg").convert(),
+                                pygame.image.load("1-8_numbers_images/4.jpg").convert(),
+                                pygame.image.load("1-8_numbers_images/5.jpg").convert(),
+                                pygame.image.load("1-8_numbers_images/6.jpg").convert(),
+                                pygame.image.load("1-8_numbers_images/7.jpg").convert(),
+                                pygame.image.load("1-8_numbers_images/8.jpg").convert()
+                      ]
     random.shuffle(symbols_face_up)
     return symbols_face_up
